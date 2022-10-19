7ddea6d095d2b7606e014ac8af6714ca75d3d52c
Thara Rajesh <thararaje@gmail.com>
2022-10-18 22:50:35 -0500

    Spa User Signup studio completed

---
+++ src/main/java/org/launchcode/spaday/controllers/userController.java
@@ -0,0 +1,33 @@
+package org.launchcode.spaday.controllers;
+
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("user")
+
+public class userController {
+    @GetMapping("add")
+    public String displayAddUserForm(){
+        return "user/add";
+    }
+
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify){
+
+        if(user.getPassword().equals(verify)){
+            return "user/index";
+        } else {
+            String error = "Passwords Must Match";
+            model.addAttribute("error", error);
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            return "user/add";
+        }
+
+    }
+
+}
